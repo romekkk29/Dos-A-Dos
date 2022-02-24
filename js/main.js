5ae43c1fbd48bf674e6ffd93fc3e1a0e11ed7738
unknown <romegomez29@gmail.com>
2022-02-24 19:49:00 -0300

    dale que va

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 
-var is_safari2 = navigator.userAgent.toLowerCase();
-var is_safari=  is_safari2.indexOf("safari");
+/* var navigatorr = navigator.userAgent.toLowerCase();
+var is_chrome=  navigatorr.indexOf("chrome");
+var is_firefox=  navigatorr.indexOf("firefox");
 let oscurece= document.querySelector("#oscurece");
 let oscurece2= document.querySelector("#oscurece2");
 let oscurece3= document.querySelector("#oscurece3");
 let principalText= document.querySelectorAll(".principalText")
 let push=0;
 let contador=0;
-let widthWindow=window.innerWidth;
-if( is_safari==-1){
+let widthWindow=window.innerWidth; */
+/* if(is_chrome || is_firefox){
  window.addEventListener("scroll",function(e){
     let offset = window.pageYOffset;
     let flecha = document.querySelector(".flecha")
@@ -77,42 +78,141 @@ if( is_safari==-1){
    
   
 })  
-}
-let verMas=document.querySelectorAll(".verMas");
-let principalTextP=document.querySelectorAll(".principalText p");
-let verMenos=document.querySelectorAll(".verMenos")
-let verProyecto=document.querySelectorAll(".verProyecto")
+} */
 
-for(let i=0;i<verMas.length;i++){
-    verMas[i].addEventListener("click",function(){
-        if(widthWindow<750){
-            principalTextP[i].style.display="block";
-            principalText[i].style.width="80%";
-            verMenos[i].style.display="block";
-            verProyecto[i].style.display="block";
-            verMas[i].style.display="none"
-        }
-        
+
+
+//menu hamburgueso
+let menuHamburgueso=document.querySelector("#menuHamburgueso");
+let cruzHambugueso=document.querySelector("#cruzHamburgueso");
+let oscuridadMobile= document.querySelector("#oscuridadMobile");
+let header= document.querySelector("header");
+let clickProyects22=document.querySelector("#clickProyects22");
+let sopa22=document.querySelector(".sopa22");
+let proyects44=document.querySelector("#proyects44")
+let sacameContact=document.querySelector("#sacameContact")
+let body=document.querySelector("html")
+menuHamburgueso.addEventListener("click",function(e){
+    e.preventDefault();
+    header.style.backgroundColor="rgba(0,0,0,0.6)"
+    header.style.backdropFilter="blur(1px)"
+    oscuridadMobile.style.marginLeft="0%"
+    menuHamburgueso.classList.add("hidden")
+    cruzHambugueso.classList.remove("hidden")
+})
+cruzHambugueso.addEventListener("click",function(e){
+    e.preventDefault();
+    header.style.backgroundColor="rgba(0,0,0,0.35)"
+    header.style.backdropFilter="blur(1px)"
+    oscuridadMobile.style.marginLeft="100%"
+    menuHamburgueso.classList.remove("hidden")
+    cruzHambugueso.classList.add("hidden")
+    body.style.overflowY="scroll"
+})
+clickProyects22.addEventListener("click",function(e){
+    e.preventDefault();
+    proyects44.classList.remove("hidden");
+    clickProyects22.classList.add("hidden")
+    sopa22.classList.remove("hidden")
+    sacameContact.classList.add("hidden")
+    body.style.overflow="hidden"
+    setTimeout( ()=>{ proyects44.style.transform="scaleY(1)"},50)
+
+})
+sopa22.addEventListener("click",function(e){
+    e.preventDefault();
+    proyects44.style.transform="scaleY(0)"
+    clickProyects22.classList.remove("hidden")
+    sopa22.classList.add("hidden")
+    body.style.overflowY="scroll"
+    setTimeout( ()=>{  proyects44.classList.add("hidden");   sacameContact.classList.remove("hidden")},1000)
+})
+
+
+
+
+
+//flechaUp 
+let flecha = document.querySelector(".flecha")
+
+  window.addEventListener("scroll",function(){
+    let widthWindow=window.innerWidth; 
+    let offset = window.pageYOffset;
+    let heightWindow= window.innerHeight/2;
+    let diferencia1= (offset/heightWindow);
+    if(offset<5 && widthWindow>750){
+        flecha.style.right="-10%";
+    }
+    if(offset<5 && widthWindow<750){
+        flecha.style.right="-15%";
+    }
+    if(diferencia1>1 && widthWindow>750){
+        flecha.style.right="20px";
+      console.log("si entro")
+    }
+    if(diferencia1>1 && widthWindow<750){
+        flecha.style.right="12px";
+    }
+  })
+
+
+// click protects 
+
+let clickProyects= document.querySelector("#clickProyects");
+let clickProyects2= document.querySelector(".sopa");
+let proyects =document.querySelector("#proyects");
+let proyectsDown= document.querySelector("#proyectsDown2");
+clickProyects.addEventListener("click",function(e){
+    e.preventDefault();
+    console.log("baja")
+    //proyects.style.marginTop="0%";
+    proyects.style.transform="scaleY(1)";
+    clickProyects.classList.add("hidden")
+    clickProyects2.classList.remove("hidden")
+    
+
+})
+clickProyects2.addEventListener("click",function(e){7
+    e.preventDefault();
+    console.log("sube")
+    //proyects.style.marginTop="-30%";
+    proyects.style.transform="scaleY(0)";
+    clickProyects2.classList.add("hidden")
+    clickProyects.classList.remove("hidden")
+})
+
+
+//texto imagen 
+
+let textoH4= document.querySelectorAll(".textoH4");
+let textoP= document.querySelectorAll(".pText");
+let signoMas=document.querySelectorAll(".signoMas")
+let signoMenos=document.querySelectorAll(".signoMenos")
+let tituloMas=document.querySelectorAll(".tituloMas")
+
+for(let i=0; i<signoMas.length;i++){
+    signoMas[i].addEventListener("click", function(){
+        tituloMas[i].style.transition="all 0.7s ease";
+        textoH4[i].style.transform="scaleY(1)";
+        textoP[i].style.transform="scaleY(1)";
+        tituloMas[i].style.transform="scaleX(0)";
+        signoMas[i].classList.add("hidden");
+        signoMenos[i].classList.remove("hidden")
+        tituloMas[i].style.backdropFilter="none"
     })
 }
-for(let i=0;i<verMas.length;i++){
-    verMenos[i].addEventListener("click",function(){
-        if(widthWindow<750){
-          
-            principalText[i].style.width="60%";
-            setTimeout(()=>{
-                principalTextP[i].style.display="none";
-            },700)
-            setTimeout(()=>{
-                verMas[i].style.display="block";
-                verMenos[i].style.display="none";
-                verProyecto[i].style.display="none";
-            },800)
-          
-        }
-        
+for(let i=0;i<signoMenos.length;i++){
+    signoMenos[i].addEventListener("click", function(){
+        textoH4[i].style.transform="scaleY(0)";
+        textoP[i].style.transform="scaleY(0)";
+        tituloMas[i].style.transition="all 0.4s ease";
+        tituloMas[i].style.transform="scaleX(1)";
+        signoMas[i].classList.remove("hidden");
+        signoMenos[i].classList.add("hidden");
+        setTimeout(()=>{
+            tituloMas[i].style.backdropFilter="blur(10px)"
+        },300)
+      
+      
     })
 }
-
-
-
